f12ba676d6943eb0683adaa515da3f861f81cb58
김유진 <kkieenex@naver.com>
2025-01-22 17:55:07 +0900

    다크모드

--- js/main.js
+++ js/main.js
@@ -3,6 +3,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const addBtn = document.querySelector(".add-btn");
     const todoList = document.querySelector(".todo-item");
     const finishList = document.querySelector(".finish-item");
+    
+    function switchOn() {
+        const mode = document.querySelector(".form-check-input");
+        const switchMode = mode.checked;
+        const main = document.querySelector(".main-wrap");
+        if (switchMode) {
+            main.classList.add("dark"); 
+        } else {
+            main.classList.remove("dark");
+        }
+    }
+    const modeSwitch = document.querySelector(".form-check-input");
+    modeSwitch.addEventListener("change", switchOn);  // 체크박스 상태 변경 시 switchOn 실행
+
+    switchOn();
 
     // 추가 버튼 클릭 이벤트
     addBtn.addEventListener("click", (event) => {
@@ -22,7 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
         newButton.addEventListener("click", () => {
             newLi.remove(); // 삭제 버튼 클릭 시 해당 li 삭제
         });
-        
+
         // i 태그 클릭이벤트
         newIcon.addEventListener("click",()=> {
             finishList.appendChild(newLi);
